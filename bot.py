452069059537b581cb4cd45378116d4c3291cfb9
MildlyGithub <hunterhancock1141@gmail.com>
2018-10-23 17:21:37 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ client = commands.Bot(command_prefix = '!')
 
 @client.event
 async def on_ready():
+    await client.change_presence(game=discord.Game(name='24/7 Gif bot'))
     print('Ready')
 
 @client.command(pass_context=True)
