203740474b08a4b46bd064d92c86ebe2d59378a5
MildlyGithub <hunterhancock1141@gmail.com>
2018-10-24 23:19:33 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ async def imgur(ctx, *args):
 async def clear(ctx, amount):
     channel = ctx.message.channel
     messages = []
-    async for message in client.logs_from(channel, limit=int(amount + 1)):
+    async for message in client.logs_from(channel, limit=int(amount)):
         messages.append(message)
     await client.delete_messages(messages)
 
