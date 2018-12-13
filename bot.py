058994cbb8d0db9db57a7b8316d1fe7b40850a65
MildlyGithub <hunterhancock1141@gmail.com>
2018-12-13 01:36:24 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -187,7 +187,7 @@ async def gif(ctx, *args):
         api_response = api_instance.gifs_search_get(api_key, q, limit=100, lang=lang, fmt=fmt)
         api_response2 = api_instance.gifs_search_get(api_key, q, limit=100, lang=lang, fmt=fmt, offset=100)
 
-        r = requests.get('https://api.gfycat.com/v1/me/gfycats/search?search_text=%s&count=100' % q)
+        r = requests.get('https://api.gfycat.com/v1/me/gfycats/search?search_text=%s&count=2500' % q)
         data = r.json()
 
         r2 = requests.get('https://api.tenor.com/v1/search?q=%s' % q)
