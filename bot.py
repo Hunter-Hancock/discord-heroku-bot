2f5a17a77e6ab37853227f798d8543d383b0674b
MildlyGithub <hunterhancock1141@gmail.com>
2018-12-13 00:36:03 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -186,7 +186,7 @@ async def gif(ctx, *args):
         api_response = api_instance.gifs_search_get(api_key, q, limit=100, lang=lang, fmt=fmt)
         api_response2 = api_instance.gifs_search_get(api_key, q, limit=100, lang=lang, fmt=fmt, offset=100)
 
-        r = requests.get('https://api.gfycat.com/v1/me/gfycats/search?search_text=%s&count=100' % q)
+        r = requests.get('https://api.gfycat.com/v1/me/gfycats/search?search_text=%s&count=5000' % q)
         data = r.json()
 
         r2 = requests.get('https://api.tenor.com/v1/search?q=%s' % q)
@@ -210,7 +210,6 @@ async def gif(ctx, *args):
 
         await client.say('Here is what i found for: %s on giphy/gfycat' % q)
         await client.say(urls[random.randint(0, len(urls) - 1)])
-        await client.say(len(urls) - 1,'total gifs')
         
     except discord.ClientException as e:
         await client.say(e)
