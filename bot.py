d7dbdc7095991cee737294b578e35cc4ec7700e9
MildlyGithub <hunterhancock1141@gmail.com>
2018-12-13 02:18:45 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -182,12 +182,11 @@ async def gif(ctx, *args):
     urls = []
 
     try:
-        starttime = time.time()
-        # Search Endpoint
+        # Search EndpointK
         api_response = api_instance.gifs_search_get(api_key, q, limit=100, lang=lang, fmt=fmt)
         api_response2 = api_instance.gifs_search_get(api_key, q, limit=100, lang=lang, fmt=fmt, offset=100)
 
-        r = requests.get('https://api.gfycat.com/v1/me/gfycats/search?search_text=%s&count=2500' % q)
+        r = requests.get('https://api.gfycat.com/v1/me/gfycats/search?search_text=%s&count=1000' % q)
         data = r.json()
 
         r2 = requests.get('https://api.tenor.com/v1/search?q=%s' % q)
@@ -211,7 +210,6 @@ async def gif(ctx, *args):
 
         await client.say('Here is what i found for: %s' % q)
         await client.say(urls[random.randint(0, len(urls) - 1)])
-        await client.say('Took %s seconds' % round(time.time() - starttime, 2))
         
     except discord.ClientException as e:
         await client.say(e)
