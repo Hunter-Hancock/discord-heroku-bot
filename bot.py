003af358c886d5c16c57ee7b3dd0b6d6ed0145d3
MildlyGithub <hunterhancock1141@gmail.com>
2018-12-29 01:55:52 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -164,7 +164,7 @@ async def bait(ctx, member : discord.Member):
 
 @client.command(pass_context=True)
 async def image(ctx):
-    await client.say(ctx.message.attachments['url'])
+    await client.say(ctx.message.attachments.url)
 
 @client.command(pass_context=True)
 async def text(ctx, number, *args):
