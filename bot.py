3b5e122f2b214747c5b8d4a7bbe0a71d192ee93d
MildlyGithub <hunterhancock1141@gmail.com>
2019-02-24 18:46:06 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ from bs4 import BeautifulSoup
 import urllib.request
 from googletrans import Translator
 
-client = commands.Bot(command_prefix = '!')
+client = commands.Bot(command_prefix='!')
 
 gfyclient_id: '2_WTCi12'
 gfyclient_secret: 'J6pITJVLezQShlLJxCqyvxJWOeFklGRepRVE6xvekVmJO69kY8H76HxEipagvA9y'
@@ -30,19 +30,22 @@ auth_token = os.environ.get('AUTH_TOKEN')
 #post_data = {"grant_type": "password", "username": "MildlyAdequateDOC", "password": os.environ.get('REDDIT_PASSWORD')}
 #headers = {"User-Agent": "discord-bot/0.1 by MildlyAdequateDOC"}
 #test_response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
-#pprint(test_response.json())
+# pprint(test_response.json())
 
-#bot_auth = '112223157440-e_P1wON56ltclGn-2Q2LkSazPwQ' # acquire token
+# bot_auth = '112223157440-e_P1wON56ltclGn-2Q2LkSazPwQ' # acquire token
+
 
 @client.event
 async def on_ready():
     await client.change_presence(game=discord.Game(name='!gif !imgur !reddit !ffz !wfa'))
 
+
 @client.command(pass_context=True)
 async def imgur(ctx, *args):
 
     search = '+'.join(str(i) for i in args)
-    res = [item for item in  imgclient.gallery_search(search, sort='top', window='all')]
+    res = [item for item in imgclient.gallery_search(
+        search, sort='top', window='all')]
 
     if (len(res) == 0):
         await client.say('Sorry no results for: %s' % search)
@@ -50,19 +53,21 @@ async def imgur(ctx, *args):
         await client.say('Here is what i found for: %s on imgur' % search)
         await client.say(res[random.randint(0, len(res))].link)
 
+
 @client.command(pass_context=True)
 async def translate(ctx, *args):
     imsg = ''.join(str(i) for i in args)
     #tmsg = translator.translate(imsg, dest=lang)
     tmsg = translator.translate(imsg, dest='en')
-    #await client.say(f'Translating from {tmsg.src} to {tmsg.dest}')
+    # await client.say(f'Translating from {tmsg.src} to {tmsg.dest}')
     await client.say(tmsg.text)
+
 
 @client.command(pass_context=True)
 async def patch(ctx, q):
     reddit = praw.Reddit(user_agent='discord-bot (by /u/MildlyAdequateDOC)',
-                    client_id=os.environ.get('REDDIT_CLIENT_ID'), client_secret=os.environ.get('REDDIT_CLIENT_SECRET'),
-                    username='MildlyAdequateDOC', password=os.environ.get('REDDIT_PASSWORD'))
+                         client_id=os.environ.get('REDDIT_CLIENT_ID'), client_secret=os.environ.get('REDDIT_CLIENT_SECRET'),
+                         username='MildlyAdequateDOC', password=os.environ.get('REDDIT_PASSWORD'))
 
     posts = []
 
@@ -71,19 +76,20 @@ async def patch(ctx, q):
             posts.append(post)
 
     embed = discord.Embed(
-        title = posts[0].title,
-        colour = discord.Colour.green())
+        title=posts[0].title,
+        colour=discord.Colour.green())
     embed.set_footer(text=posts[0].url)
 
     await client.say('Here is the latest patch notes for %s.' % q)
-    #await client.say(embed=embed)
+    # await client.say(embed=embed)
     await client.say(posts[0].url)
+
 
 @client.command(pass_context=True)
 async def reddit(ctx, *args):
     reddit = praw.Reddit(user_agent='discord-bot (by /u/MildlyAdequateDOC)',
-                    client_id=os.environ.get('REDDIT_CLIENT_ID'), client_secret=os.environ.get('REDDIT_CLIENT_SECRET'),
-                    username='MildlyAdequateDOC', password=os.environ.get('REDDIT_PASSWORD'))
+                         client_id=os.environ.get('REDDIT_CLIENT_ID'), client_secret=os.environ.get('REDDIT_CLIENT_SECRET'),
+                         username='MildlyAdequateDOC', password=os.environ.get('REDDIT_PASSWORD'))
     q = ''.join(str(i) for i in args)
     posts = []
 
@@ -96,13 +102,14 @@ async def reddit(ctx, *args):
     random.seed(datetime.datetime.now().time())
     wadu = posts[random.randint(0, length)]
     embed = discord.Embed(
-        title = wadu.title,
-        description = wadu.selftext,
-        colour = discord.Colour.green())
+        title=wadu.title,
+        description=wadu.selftext,
+        colour=discord.Colour.green())
     embed.set_image(url=wadu.url)
     embed.set_footer(text='https://www.reddit.com/comments/%s' % wadu.id)
     await client.say(embed=embed)
     await client.say('https://www.reddit.com/comments/%s' % wadu.id)
+
 
 @client.command(pass_context=True)
 async def avatar(ctx):
@@ -111,6 +118,7 @@ async def avatar(ctx):
         await client.say('https://cdn.discordapp.com/embed/avatars/0.png')
     else:
         await client.say(user.avatar_url)
+
 
 @client.command(pass_context=True)
 async def scrape(ctx, url, tag, class_=None):
@@ -131,26 +139,28 @@ async def scrape(ctx, url, tag, class_=None):
         await client.say(text[random.randint(0, len(text) - 1)])
     if tag == 'image':
         for img in soup.find_all('img'):
-           urls.append(img['src'])
+            urls.append(img['src'])
         await client.say(urls[random.randint(0, len(urls) - 1)])
 
 
 @client.command(pass_context=True)
 async def wfa(ctx, *args):
-    id=os.environ.get('WFA_ID')
+    id = os.environ.get('WFA_ID')
     q = ' '.join(str(i) for i in args)
     if '+' in q:
         q = q.replace('+', 'plus')
-    r = requests.get('http://api.wolframalpha.com/v2/query?appid=%s&input=%s&format=plaintext&output=json' % (id, q))
+    r = requests.get(
+        'http://api.wolframalpha.com/v2/query?appid=%s&input=%s&format=plaintext&output=json' % (id, q))
     data = r.json()
     await client.say(data['queryresult']['pods'][1]['subpods'][0]['plaintext'])
 
+
 @client.command(pass_context=True)
-async def bait(ctx, member : discord.Member):
+async def bait(ctx, member: discord.Member):
     jebaits = []
 
-    r = requests.get('https://manga.tokyo/')
-    content = r.text
+    # r = requests.get('https://manga.tokyo/')
+    # content = r.text
     r2 = requests.get('https://myanimelist.net/news')
     content2 = r2.text
     r3 = requests.get('https://www.crunchyroll.com/news')
@@ -158,25 +168,24 @@ async def bait(ctx, member : discord.Member):
 
     text = []
 
-    soup = BeautifulSoup(content, 'html.parser')
+    # soup = BeautifulSoup(content, 'html.parser')
     soup2 = BeautifulSoup(content2, 'html.parser')
     soup3 = BeautifulSoup(content3, 'html.parser')
-    for p in soup.find_all('h3', class_='post-list-content-ttl'):
-        text.append(p.text)
+    # for p in soup.find_all('h3', class_='post-list-content-ttl'):
+    #     text.append(p.text)
     for n in soup2.find_all('div', class_='text'):
         text.append(n.text)
     for c in soup3.find_all('h2'):
         text.append(c.get_text())
 
-    jebaits.append('BRAND SPANKIN NEW ANIME STRAIGHT OFF THE JAPANESE PRINTERS ELECTRONICALLY DIGITIZED INTO CRISP HIGH DEFINITION PIXEL JAPANESE GOODNESS!!!')
-    jebaits.append('HOLY MOLY http://hentaihaven.org/ IS COMING BACK NO FUCKING JOKE')
+    jebaits.append(
+        'BRAND SPANKIN NEW ANIME STRAIGHT OFF THE JAPANESE PRINTERS ELECTRONICALLY DIGITIZED INTO CRISP HIGH DEFINITION PIXEL JAPANESE GOODNESS!!!')
+    jebaits.append(
+        'HOLY MOLY http://hentaihaven.org/ IS COMING BACK NO FUCKING JOKE')
     jebaits.extend(text)
 
     await client.say(f'{member.mention}{jebaits[random.randint(0, len(jebaits) - 1)]}')
 
-@client.command(pass_context=True)
-async def image(ctx):
-    await client.say(ctx.message.attachments)
 
 @client.command(pass_context=True)
 async def text(ctx, number, *args):
@@ -193,52 +202,53 @@ async def text(ctx, number, *args):
         body=' '.join(str(i) for i in args)
     )
 
-@client.command(pass_context=True)
-async def ffz(ctx, q):
-    #starttime = time.time()
-    if(q == 'monkaS'):
-        embed = discord.Embed(
-        title = 'monkaS',
-        colour = discord.Colour.green())
-        embed.set_image(url='https://cdn.frankerfacez.com/7ed3da04c09547097595ff979e629c36.png')
-        await client.say(embed=embed)
-    elif(q == None):
-        await client.say('Enter an arguement.')
-    elif(q == 'hypers'):
-        embed = discord.Embed(
-        title = 'hypers',
-        colour = discord.Colour.green())
-        embed.set_image(url='https://cdn.frankerfacez.com/2eca1ebdd82e120d31ab3b59e6aea68b.png')
-        await client.say(embed=embed)
-    elif(q == 'pepehands'):
-        embed = discord.Embed(
-        title = 'pepehands',
-        colour = discord.Colour.green())
-        embed.set_image(url='https://cdn.frankerfacez.com/b97ed9ea44a548134578aecd47348784.png')
-        await client.say(embed=embed)
-    else:
-        url = 'https://api.frankerfacez.com/v1/emoticons?q=%s&sort=count-desc' % q
-        s = requests.Session()
-        r = s.get(url).json()
-        # r = requests.get(url).json()
+# @client.command(pass_context=True)
+# async def ffz(ctx, q):
+#     #starttime = time.time()
+#     if(q == 'monkaS'):
+#         embed = discord.Embed(
+#         title = 'monkaS',
+#         colour = discord.Colour.green())
+#         embed.set_image(url='https://cdn.frankerfacez.com/7ed3da04c09547097595ff979e629c36.png')
+#         await client.say(embed=embed)
+#     elif(q == None):
+#         await client.say('Enter an arguement.')
+#     elif(q == 'hypers'):
+#         embed = discord.Embed(
+#         title = 'hypers',
+#         colour = discord.Colour.green())
+#         embed.set_image(url='https://cdn.frankerfacez.com/2eca1ebdd82e120d31ab3b59e6aea68b.png')
+#         await client.say(embed=embed)
+#     elif(q == 'pepehands'):
+#         embed = discord.Embed(
+#         title = 'pepehands',
+#         colour = discord.Colour.green())
+#         embed.set_image(url='https://cdn.frankerfacez.com/b97ed9ea44a548134578aecd47348784.png')
+#         await client.say(embed=embed)
+#     else:
+#         url = 'https://api.frankerfacez.com/v1/emoticons?q=%s&sort=count-desc' % q
+#         s = requests.Session()
+#         r = s.get(url).json()
+#         # r = requests.get(url).json()
 
-    embed = discord.Embed(
-        title = q,
-        colour = discord.Colour.green())
+#     embed = discord.Embed(
+#         title = q,
+#         colour = discord.Colour.green())
 
-    try:
-        try:
-            res = 'https:',(r['emoticons'][0]['urls']['4'])
-            embed.set_image(url=''.join(res))
-            await client.say(embed=embed)
-        except KeyError:
-            res = 'https:',(r['emoticons'][0]['urls']['2'])
-            embed.set_image(url=''.join(res))
-            await client.say(embed=embed)
-    except KeyError:
-        res = 'https:',(r['emoticons'][0]['urls']['1'])
-        embed.set_image(url=''.join(res))
-        await client.say(embed=embed)
+#     try:
+#         try:
+#             res = 'https:',(r['emoticons'][0]['urls']['4'])
+#             embed.set_image(url=''.join(res))
+#             await client.say(embed=embed)
+#         except KeyError:
+#             res = 'https:',(r['emoticons'][0]['urls']['2'])
+#             embed.set_image(url=''.join(res))
+#             await client.say(embed=embed)
+#     except KeyError:
+#         res = 'https:',(r['emoticons'][0]['urls']['1'])
+#         embed.set_image(url=''.join(res))
+#         await client.say(embed=embed)
+
 
 @client.command(pass_context=True)
 async def clear(ctx, amount):
@@ -247,6 +257,7 @@ async def clear(ctx, amount):
     async for message in client.logs_from(channel, limit=int(amount)):
         messages.append(message)
     await client.delete_messages(messages)
+
 
 @client.command(pass_context=True)
 async def gal(ctx, s=3):
@@ -265,6 +276,7 @@ async def gal(ctx, s=3):
     if s == 7:
         await client.say('https://bestofcomicbooks.com/wp-content/uploads/2018/06/gal-gadot-fantastic.gif')
 
+
 @client.command(pass_context=True)
 async def gif(ctx, *args):
     # create an instance of the API class
@@ -281,7 +293,8 @@ async def gif(ctx, *args):
         # api_response = api_instance.gifs_search_get(api_key, q, limit=100, lang=lang, fmt=fmt)
         # api_response2 = api_instance.gifs_search_get(api_key, q, limit=100, lang=lang, fmt=fmt, offset=100)
 
-        r = requests.get(f'https://api.gfycat.com/v1/me/gfycats/search?search_text={q}&count=1000')
+        r = requests.get(
+            f'https://api.gfycat.com/v1/me/gfycats/search?search_text={q}&count=1000')
         data = r.json()
 
         # r2 = requests.get('https://api.tenor.com/v1/search?q=%s' % q)
@@ -291,7 +304,7 @@ async def gif(ctx, *args):
         # while l < len(data2['results']) - 1:
         #     urls.append(data2['results'][l]['url'])
         #     l += 1
-        
+
         k = 0
         length = len(data['gfycats']) - 1
         while k < length:
@@ -306,7 +319,7 @@ async def gif(ctx, *args):
 
         await client.say('Here is what i found for: %s' % q)
         await client.say(urls[random.randint(0, len(urls) - 1)])
-        
+
     except:
         await client.say('Sumtin fucked up gimme sec')
         os.system('heroku restart -a discord-heroku-bot')
