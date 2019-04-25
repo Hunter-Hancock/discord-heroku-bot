72cadb3e3322f539cfd8403305c106b82a6a440c
MildlyGithub <hunterhancock1141@gmail.com>
2019-04-24 20:39:46 -0500

    Update spoilers.py

--- spoilers.py
+++ spoilers.py
@@ -5,10 +5,17 @@ import os
 bot = Bot(command_prefix='!')
 
 spoiler_text = ['Avengers', 'Endgame', 'Iron man', 'dies', 'captain america', 'ant man', 'thanos'].strip().lower()
+
+s = [e.strip().split().lower() for e in spoiler_text]
+
+@bot.event
+async def on_ready():
+    await client.change_presence(game=discord.Game(name='NO AVENGERS SPOILERS!'))
+
 @bot.event
 async def on_message(message):
     message_content = message.content.strip().lower()
-    if any(spoiler in message_content for spoiler in spoiler_text):
+    if any(spoiler in message_content for spoiler in s):
         await bot.send_message(message.channel, 'No spoilers bud')
         await bot.delete_message(message)
 bot.run(os.environ.get('BOT_TOKEN'))
