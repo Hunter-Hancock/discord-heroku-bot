829df2ea725efefa69e60b90a9925d2d1717516d
MildlyGithub <hunterhancock1141@gmail.com>
2020-03-16 03:55:07 -0500

    Update status.py

--- cogs/status.py
+++ cogs/status.py
@@ -8,9 +8,9 @@ class Status(commands.Cog):
     def __init__(self, client):
         self.client = client
     
-    # @commands.Cog.listener()
-    # async def on_ready(self):
-    #    self.update_status.start()
+    @commands.Cog.listener()
+    async def on_ready(self):
+       self.update_status.start()
 
     @commands.command()
     async def status(self, ctx, ip: str):
@@ -21,11 +21,16 @@ class Status(commands.Cog):
         except Exception as e:
             print(e)
         
-    # @tasks.loop(seconds=5)
-    # async def update_status(self):
-    #     server = MinecraftServer.lookup('68.63.192.222:25565')
-    #     status = server.status()
-    #     await client.change_presence(activity=discord.Game(f'{status.players.online}/5 players on server'))
+    @tasks.loop(seconds=60)
+    async def update_status(self):
+        try:
+            server = MinecraftServer.lookup('68.63.192.222')
+            status = server.status()
+            await client.change_presence(activity=discord.Game(f'Enigmatica 2 Expert: {status.players.online}/4 players on server'))
+        except Exception:
+            game = discord.Game('!gif !imgur !reddit !nsfw')
+            await self.client.change_presence(status=discord.Status.online, activity=game)
+        
 
 def setup(client):
     client.add_cog(Status(client))
