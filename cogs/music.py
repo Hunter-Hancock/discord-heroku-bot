73d6265b3eb4b2abb3313fd7caa4dde38a608344
MildlyGithub <hunterhancock1141@gmail.com>
2020-03-15 23:11:07 -0500

    added music

--- cogs/music.py
+++ cogs/music.py
@@ -8,7 +8,6 @@ class Music(commands.Cog):
 
     def __init__(self, client):
         self.client = client
-        discord.opus.load_opus('opus-1.3.1')
 
     @commands.command(aliases=['j'])
     async def join(self, ctx):
@@ -16,10 +15,57 @@ class Music(commands.Cog):
         channel = ctx.message.author.voice.channel
         voice = get(self.client.voice_clients, guild=ctx.guild)
 
+        
+
+    @commands.command(aliases=['l'])
+    async def stop(self, ctx):
+        channel = ctx.message.author.voice.channel
+        voice = get(self.client.voice_clients, guild=ctx.guild)
+
+        if voice and voice.is_connected():
+            await voice.disconnect()
+        else:
+            voice = await channel.disconnect()
+    
+    @commands.command(aliases=['p'])
+    async def play(self, ctx, url: str):
+
         if voice and voice.is_connected():
             await voice.move_to(channel)
         else:
             voice = await channel.connect()
+        
+        await ctx.send(f'Joined {channel}')
+
+        song_there = os.path.isfile("song.mp3")
+        try:
+            if song_there:
+                os.remove("song.mp3")
+        except PermissionError:
+            return
+        
+        voice = get(self.client.voice_clients, guild=ctx.guild)
+
+        ytdl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192'
+            }]
+        }
+
+        with youtube_dl.YoutubeDL(ytdl_opts) as ydl:
+            print('Downloading song\n')
+            ydl.download([url])
+
+        for file in os.listdir('./'):
+            if file.endswith('.mp3'):
+                name = file
+                print(f'Renamed File: {file}')
+                os.rename(file, 'song.mp3')
+
+        await voice.play(discord.FFmpegPCMAudio('song.mp3'), after=lambda e: await voice.disconnect())
 
 def setup(client):
     client.add_cog(Music(client))
