3bfad7a0cb7ec7a4a05213cdb0b31cd97495dbd2
MildlyGithub <hunterhancock1141@gmail.com>
2020-04-06 02:56:24 -0500

    Update presence.py

--- cogs/presence.py
+++ cogs/presence.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import datetime as dt
 
 class Presence(commands.Cog):
@@ -7,18 +7,26 @@ class Presence(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    @commands.command()
+    async def check_online(self, ctx):
+        me = self.client.get_user(102817191446982656)
+
     @commands.Cog.listener()
     async def on_member_update(self, oldMember, newMember):
         if oldMember.status != newMember.status:
-            sendDM()
+            await self.sendDM(oldMember, newMember)
 
-    @staticmethod
-    async def sendDM():
-        me = self.client.get_user('102817191446982656')
-        
+    async def sendDM(self, oldMember, newMember):
+        channel = self.client.get_channel(225748465714462721)
+
+        server = self.client.get_guild(225748465714462721)
+
+        role = discord.utils.find(lambda r: r.name == 'WEEB', server.roles)
+
         hr = dt.datetime.today().hour
-        if hr > 12 and hr < 24:
-            (await me).send('test')
+        if role in newMember.roles:
+            if (hr > 12 and hr < 24) and (str(newMember.status) == 'online'):
+                await channel.send(f'{newMember.name} is {newMember.status}')
 
 def setup(client):
     client.add_cog(Presence(client))
